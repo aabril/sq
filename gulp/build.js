b1ffaeab2ecf8d21d79afe52a68ad359ecf1aff2
Albert Abril <albert.abril@gmail.com>
2015-07-21 12:18:43 +0200

    build > stylus and cofee

--- gulp/build.js
+++ gulp/build.js
@@ -1,26 +1,32 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
-// require('gulp-stylus');
-// require('gulp-stylus');
+var stylus = require('gulp-stylus');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
 
 var build = {};
 
 build.jade = function(){
   var locals = {};
   var paths = [ 'src/jade/**jade' ];
-  return gulp
-            .src(paths)
-            .pipe(jade({ locals: locals , pretty: true }))
-            .pipe(gulp.dest('./public/'));
+  return gulp.src(paths)
+             .pipe(jade({ locals: locals , pretty: true }))
+             .pipe(gulp.dest('./public/'));
 };
 build.stylus = function(){
-
+   var paths = [ 'src/styl/*styl'];
+   return gulp.src(paths).pipe(stylus()).pipe(gulp.dest('./public/'));
 };
 build.coffee = function(){
-
+  return  gulp.src('./src/coffee/*.coffee')
+              .pipe(coffee({bare: true}).on('error', gutil.log))
+              .pipe(gulp.dest('./public/'))
 };
 build.img = function(){
-
+  var paths = [ 'src/img/**'];
+  return gulp
+        .src(paths)
+        .pipe(gulp.dest('./public/'));
 };
 
 
---
+++ gulp/config.js
@@ -0,0 +1,18 @@
+var config = {};
+config.paths = {};
+
+config.paths.build = {
+  "jade"   :[],
+  "stylus" :[],
+  "img"    :[],
+  "coffee" :[]
+};
+
+config.paths.watch = {
+  "jade"   :[],
+  "stylus" :[],
+  "img"    :[],
+  "coffee" :[]
+};
+
+module.exports = config;
