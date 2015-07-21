f3fa71953118343a092006652b6aa361b6971351
Albert Abril <albert.abril@gmail.com>
2015-07-21 12:38:20 +0200

    Typo2

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ var browserSync = require('browser-sync');
 gulp.task('build:jade', tasks.build.jade);
 gulp.task('build:stylus', tasks.build.stylus);
 gulp.task('build:coffee', tasks.build.coffee);
-gulp.task('build:img', tasks.build.img)
-;
+gulp.task('build:img', tasks.build.img);
+
 gulp.task('build', ['build:jade','build:stylus', 'build:coffee', 'build:img']);
 
 gulp.task('loop', tasks.loop);
