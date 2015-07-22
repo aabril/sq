d0d95837296329356c0be580378965c7b4bb0544
Albert Abril <albert.abril@gmail.com>
2015-07-22 13:30:27 +0200

    stylus -> google fonts -> variables -> automated

--- gulp/loop.js
+++ gulp/loop.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
 module.exports = function() {
-      // gulp.watch(['src/img/**'], ['images']);
-      gulp.watch(['src/*.jade','src/jade/**/*.jade'], ['build']);
-      // gulp.watch(['src/less/*.less','src/less/**/*.less'], ['styles']);
+      gulp.watch(['src/img/**'], ['build:img']);
+      gulp.watch(['src/*.jade','src/jade/**/*.jade'], ['build:jade']);
+      gulp.watch(['src/styl/*.styl','src/styl/**/*.styl'], ['build:stylus']);
       // gulp.watch(['src/js/*','src/js/**/*'], ['scripts']);
 };
