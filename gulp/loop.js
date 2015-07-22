b17622e5f1a16b76a1b9fe498c4d526027c404d5
Albert Abril <albert.abril@gmail.com>
2015-07-22 12:18:33 +0200

    hover effects ideas : set1 and set2

--- gulp/loop.js
+++ gulp/loop.js
@@ -3,7 +3,7 @@ var browserSync = require('browser-sync');
 
 module.exports = function() {
       // gulp.watch(['src/img/**'], ['images']);
-      gulp.watch(['src/*.jade','src/jade/**/*.jade'], ['build:root']);
+      gulp.watch(['src/*.jade','src/jade/**/*.jade'], ['build']);
       // gulp.watch(['src/less/*.less','src/less/**/*.less'], ['styles']);
       // gulp.watch(['src/js/*','src/js/**/*'], ['scripts']);
 };
